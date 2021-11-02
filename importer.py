d2f9cca1e6e0ae93d716c597f36cc023a4d53b08
Mateusz Praski <mateusz.praski@gmail.com>
2021-11-02 19:41:50 +0100

    Added IGA and rIGA matrix with importer

---
+++ importer.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def read_file(filename: str) -> np.ndarray:
+    with open(filename, 'r') as f:
+        ndims = list(map(int, f.readline().split()))
+
+        matrix = np.empty(ndims, dtype=float)
+
+        for line in f:
+            if line == '\n':
+                break
+            x, y, val = list(line.split())
+            x = int(x)
+            y = int(y)
+            val = float(val)
+            matrix[x - 1, y - 1] = val 
+
+    return matrix
+
+def resize_matrix(matrix: np.ndarray, desired_size: int) -> np.ndarray:
+    new_matrix = np.empty((desired_size, desired_size), dtype=float)
+    block_size = matrix.shape[0]
+    times = desired_size // block_size
+    for i in range(times):
+        for j in range(times):
+            new_matrix[i * block_size : (1 + i) * block_size, j * block_size : (j + 1) * block_size] = matrix
+    return new_matrix
+
+A = read_file('iga.txt')
+print(A)
+print(resize_matrix(A, 32))
